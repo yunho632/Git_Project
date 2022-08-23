cbfd3deb033ab6574853f8b28dc39daddf250b3a
yunho632 <111098293+yunho632@users.noreply.github.com>
2022-08-24 07:45:25 +0900

    frmlogin

---
+++ frmLoading.vb
@@ -0,0 +1,18 @@
+﻿Public Class frmLoading
+
+    Private Sub frmLoading_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        ' PictureBox1.Image = Image.FromFile(Application.StartupPath + "\Loading.png")
+    End Sub
+
+    Public Sub New()
+
+        ' 이 호출은 디자이너에 필요합니다.
+        InitializeComponent()
+
+        ' InitializeComponent() 호출 뒤에 초기화 코드를 추가하십시오.
+        tablayoutLoading.Parent = PictureBox1
+        lbMainTitle.Parent = tablayoutLoading
+        picMaker.Image = My.Resources.MiraeVisionLogo ' Image.FromFile(Application.StartupPath + "\CI.png")
+        picCustomerCI.Image = My.Resources.HanonLogo ' Image.FromFile(Application.StartupPath + "\logo.png")
+    End Sub
+End Class
