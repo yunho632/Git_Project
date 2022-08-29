a9114587063d9436d19b3945cc4a764aeed481bf
yunho632 <111098293+yunho632@users.noreply.github.com>
2022-08-29 15:40:25 +0900

    Add files via upload
    03

---
+++ Form_CameraConfig.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WVision
+{
+    public class Config
+    {
+        private string ActiveFile = "";
+        public string ConfigXmlName = "";
+
+        private object LockObject = new object();
+
+        public Config()
+        {
+
+        }
+        public Config(string pConfigXmlName)
+        {
+            this.ConfigXmlName = pConfigXmlName;
+            this.UpdateConfig();
+        }
+
+        private void UpdateCameras()
+        {
+            {
+                if (this.ActiveFile == "") return;
+                Config configCameras = new Config();
+
+
+                configCameras.ConfigXmlName = this.ActiveFile;
+                configCameras.UpdateConfig();
+               // this.dataGridView_Camera.RowCount = 0;
+
+                //if (configCameras.GetSectionGroup("CameraList") == null)
+                //{
+                //    configCameras.AddSectionGroup("", "CameraList");
+                //}
+                //else
+                //{
+                //    foreach (System.Configuration.ConfigurationSection Section in configCameras.GetAllSection("CameraList"))
+                //    {
+
+                //        string[] RowData = new string[this.dataGridView_Camera.Columns.Count];
+                //        for (int iCol = 0; iCol < this.dataGridView_Camera.ColumnCount; iCol++)
+                //        {
+                //            RowData[iCol] = configCameras.GetValue(Section.SectionInformation.SectionName, this.dataGridView_Camera.Columns[iCol].HeaderText.ToString(), "").ToString();
+                //        }
+                //        this.dataGridView_Camera.Rows.Add(RowData);
+
+                //    }
+                //}
+
+
+                //this.Text = string.Format("{0} - {1}", "Camera List", System.IO.Path.GetFileName(this.ActiveFile));
+            }
+
+
+
+        }
+        public void UpdateConfig()
+        {
+            try
+            {
+                lock (LockObject)
+                {
+                    //this.config = ConfigurationManager.OpenMappedExeConfiguration(new ExeConfigurationFileMap { ExeConfigFilename = ConfigXmlName }, ConfigurationUserLevel.None);
+                }
+            }
+            catch (Exception ex)
+            {
+               
+                //this.config = null;
+            }
+
+        }
+
+
+    }
+
+    public partial class Form_CameraConfig : Form
+    {
+        public Form_CameraConfig()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
+
---
+++ IVisionCamera.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Cognex.VisionPro;
+//using Cognex.VisionPro.FGGigE;
+//using Cognex.VisionPro.ImageProcessing;
+
+namespace WVision
+{
+    public enum enum_CameraGrabMode : short
+    {
+        Manual = 0,
+        Auto = 1,
+        Live = 2
+    };
+
+    public interface IVisionCamera : IDisposable
+    {
+        int SEQ { get; set; } // 카메라 보여주는 순서?
+        string CameraName { get; set; } // 내부용 확인용        
+        string SerialNo { get; set; } // Cognex Serial No
+        enum_CameraGrabMode CameraMode { get; set; }
+        DateTime AcquireTime { get; set; }
+        //CogIPOneImageFlipRotateOperationConstants FlipRotate { get; set; }
+        long AcqTime { get; set; }
+        //bool initialization();
+        void Snapshot();
+        void CameraAuto();
+        void CameraManaual();
+        void CameraLive();
+        bool IsAlive(); // 카메라 상태        
+    }
+
+    public class WindyCameraEventArgs : EventArgs
+    {
+        public string SerialNumber { get; set; }
+        public ICogImage Image { get; set; }
+        public WindyCameraEventArgs(string serialNumber, ICogImage image)
+        {
+            this.SerialNumber = serialNumber;
+            this.Image = image;
+        }
+    }
+}
